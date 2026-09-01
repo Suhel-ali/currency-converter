998fcee524b1bf25da1d9b6a16154c19e0ffc858
Sohel <joyysohel@gmail.com>
2026-09-01 09:40:34 +0530

    3rd commit for cleanness

--- converter/models.py
+++ converter/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
         upload_to="profile_images/",
-        default="profile_images/default.png"
+        default="profile_images/2.jpg"
     )
 
     def __str__(self):
